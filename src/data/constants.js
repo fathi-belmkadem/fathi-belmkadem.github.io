999b0ca7343a261a8ead50e09f5cd48a0b6c7702
fathi <fathi.belmkadem@supcom.tn>
2024-07-15 17:53:33 +0100

    1st commit

---
+++ src/data/constants.js
@@ -0,0 +1,432 @@
+export const Bio = {
+  name: "Fathi BELMKADEM",
+  roles: [
+    "ICT Engineering Student",
+    "Penetration Tester",
+    "ISC² member",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/fathi-belmkadem",
+  resume:
+    "https://drive.google.com/file/d/1EdJYqCcsXp1zunXWShTs-xDdzZK8yPPR/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/fathi-belmkadem/",
+  twitter: "https://x.com/FBelmkadem94269",
+  insta: "https://www.instagram.com/fathi.mk/",
+  facebook: "https://www.facebook.com/rxnrhoee/",
+};
+
+export const skills = [
+  {
+    title: "Tools & Frameworks",
+    skills: [
+      {
+        name: "Nmap",
+        image: "https://vulners.com/static/img/nmap.webp",
+      },
+      {
+        name: "Metasploit",
+        image:
+          "https://w7.pngwing.com/pngs/410/153/png-transparent-white-hat-metasploit-project-exploit-computer-security-penetration-test-framework-miscellaneous-logo-payload.png",
+      },
+      {
+        name: "Empire ",
+        image: "https://cdn.cyberpunk.rs/wp-content/uploads/2020/04/empire_logo_black.png",
+      },
+      {
+        name: "Burpsuite",
+        image:
+          "https://th.bing.com/th/id/R.c70df165577fdb7162aeee4360110650?rik=jQNvU9VWj1QNpw&riu=http%3a%2f%2fimg.informer.com%2ficons_mac%2fpng%2f128%2f404%2f404676.png&ehk=LJTj48VXQZMp6%2fUrDWMbCAOHSBMWCv7UfHROUySTpKE%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1",
+      },
+      {
+        name: "Nessus",
+        image:
+          "https://www.tenable.com/sites/drupal.dmz.tenablesecurity.com/files/images/blog/logo_Nessus_FullColor_RGB-01.png",
+      },
+      {
+        name: "ZAP",
+        image:
+          "https://appstore.edgenexus.io/wp-content/uploads/2022/08/Group-26.jpg",
+      },
+      {
+        name: "Wireshark",
+        image:
+          "https://th.bing.com/th/id/R.1bc7df9d7188dc1703c2efcf20929948?rik=IstBNNwMade%2frw&pid=ImgRaw&r=0",
+      },
+      {
+        name: "Nuclei",
+        image:
+          "https://th.bing.com/th/id/OIP.zDaZr4YJjD0hks7kRW9XAAHaHa?rs=1&pid=ImgDetMain",
+      },
+    ],
+  },
+  {
+    title: "Programmming Languages",
+    skills: [
+      {
+        name: "C",
+        image:
+          "https://www.pngitem.com/pimgs/m/31-312155_c-programming-language-logo-hd-png-download.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png",
+      },
+      {
+        name: "Nim",
+        image:
+          "https://th.bing.com/th/id/OIP.R-GtonNAScyBSMT1EvzV0QHaF8?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Assembly",
+        image:
+          "https://th.bing.com/th/id/OIP.bXRcFDecaRZEm18vNb9BSgHaHa?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://pngimg.com/uploads/php/php_PNG23.png",
+      },
+      {
+        name: "Javascript",
+        image:
+          "https://th.bing.com/th/id/OIP.9jAkFPGpe5YO-8RM0Le_XgHaIa?rs=1&pid=ImgDetMain",
+      },
+
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://toppng.com/uploads/preview/jenkins-logo-11609365847mufysaivph.png",
+      },
+      {
+        name: "Nginx",
+        image: "https://download.logo.wine/logo/Nginx/Nginx-Logo.wine.png",
+      },
+      // {
+      //   name: "Grafana",
+      //   image:
+      //     "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Grafana_logo.svg/1200px-Grafana_logo.svg.png",
+      // },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/00/Kubernetes_%28container_engine%29.png",
+      },
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      // {
+      //   name: "Prometheus",
+      //   image:
+      //     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Prometheus_software_logo.svg/1200px-Prometheus_software_logo.svg.png",
+      // },
+    ],
+  },
+  {
+    title: "Security Frameworks & Standards",
+    skills: [
+      {
+        name: "NIST",
+        image:
+          "https://datainnovation.org/wp-content/uploads/2019/05/nist-logo.png",
+      },
+      {
+        name: "ISO 27001",
+        image:
+          "https://th.bing.com/th/id/R.5416f6295e7a275616721d96453c7daf?rik=N8IiJmqRZ9p9IQ&riu=http%3a%2f%2fww1.prweb.com%2fprfiles%2f2016%2f10%2f10%2f13751604%2fISO_27001_Final+Logo.jpg&ehk=8q%2fn85%2b%2fwWzyGtU%2bhte3SURZ8OBQmmPKtIfKkeykpag%3d&risl=&pid=ImgRaw&r=0",
+      },
+      {
+        name: "CIS Controls",
+        image:
+          "https://th.bing.com/th/id/OIP.XC1F4f9s6TifNMx3_QAqdQHaHa?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "OSSTMM",
+        image:
+          "https://www.ubcom.eu/storage/2023/01/Logo-OSSTMM.png",
+      },
+      {
+        name: "PTES",
+        image: "https://th.bing.com/th/id/OIP.Tbuyxv5lzWX8USb-Vivj0wHaFj?rs=1&pid=ImgDetMain",
+      }
+    ],
+  },
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Sk Learn Kit",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Networking",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/005/267/602/non_2x/business-network-white-icon-vector.jpg",
+      },
+      {
+        name: "Malware Analysis",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/014/784/074/non_2x/virus-attack-icon-free-vector.jpg",
+      },
+      {
+        name: "Automation",
+        image:
+          "https://icon-library.com/images/automation-icon-png/automation-icon-png-21.jpg",
+      },
+      {
+        name: "SOAR & SIEM",
+        image:
+          "https://th.bing.com/th/id/OIP.uR50pqfHPYWJ-AP4CL9HlwAAAA?w=271&h=271&rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Vulnerability Assessment",
+        image:
+          "https://cdn.iconscout.com/icon/premium/png-256-thumb/vulnerability-assessment-2011942-1692265.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/OIP.c699w8NsMtkI5xKeGTqLcwAAAA?rs=1&pid=ImgDetMain",
+    role: "End of Study Intern",
+    company: "Responsible-cyber, Hybrid",
+    date: "February 2024 - August 2024",
+    desc: "Developed intelligent systems that can identify, analyze, and potentially exploit vulnerabilities in a manner similar to human security experts, but at scale and speed that humans cannot match. Developed automated scripts to enhance the efficiency and accuracy of the penetration testing process, significantly reducing the likelihood of human errors. Designed a system for automated or manual operation via user-directed scripts or an intelligent agent.",
+    skills: [
+      "Reinforcement Learning",
+      "Penetration Testing",
+      "Automation",
+      "Vulnerability Assessment",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4E0BAQGg4XVAihfuwQ/company-logo_200_200/0/1637245650699/cytekia_logo?e=2147483647&v=beta&t=p9S4L69QQM4KAmPHXtX74Hg_HAOqTDX5xcmGnCVh7f0",
+    role: "Intern",
+    company: "Cytekia, Tunisia",
+    date: "June 2023 - August 2023",
+    desc: "Implemented machine learning algorithms within the Cuckoo Sandbox environment, enhancing evasive malware detection accuracy by 90% and reducing false positives by 15%. Increased threat detection by 40% with machine learning models that adapt to unknown threats. Developed and implemented a robust and scalable architecture to safeguard the environment from advanced malware attacks.",
+    skills: [
+      "Cuckoo Sandbox",
+      "Python",
+      "Volatility",
+      "WinAPI",
+      "YARA",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/433947165_815262507300383_1888612990411191086_n.png?alt=media&token=0e605a27-de5e-4408-bc43-b011a68be415",
+    role: "Manager",
+    company: "Hackfest Event",
+    date: "June 2023",
+    desc: "Led the planning and execution of the Hackfest competition. Developed the comprehensive event program, outlining competition details, categories, and schedules.",
+    skills: [
+      "Event Planning",
+      "Project Management",
+      "Team Leadership",
+      "Communication",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/307307484_1121200635472383_7386688548292024517_n.jpeg?alt=media&token=516c74e0-c0c5-49e4-aac8-46d405bdfdc6",
+    role: "Treasurer",
+    company: "Sup'com Cybersecurity Club",
+    date: "September 2022 - July 2023",
+    desc: "Helped the club in implementing a new strategy in response to the dynamic changes in cybersecurity. Managed various operations including workshops and events.",
+    skills: [
+      "Strategic Planning",
+      "Event Management",
+      "Leadership",
+      "Budgeting",
+
+    ],
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/unnamed.jpg?alt=media&token=d41a2d31-fa1b-4901-9f91-54f289989420",
+    role: "Intern",
+    company: "National Agency for Cybersecurity, Tunisia",
+    date: "June 2022 - July 2022",
+    desc: "Formulated machine learning algorithms to enhance network security surveillance. Engineered models capable of analyzing network traffic patterns and behavior, leading to a 60% increase in accurate threat identification and mitigation. Improved network defenses by providing real-time threat detection and quick reaction capabilities.",
+    skills: [
+      "Python",
+      "Machine Learning",
+      "TensorFlow",
+      "Wireshark",
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 5,
+    img: "https://th.bing.com/th/id/OIP.OyHuKE6Ae3EPKudEHOuqhwHaHa?rs=1&pid=ImgDetMain",
+    school: "Higher School Of Communication of Tunis",
+    date: "Sep 2021 - Present",
+    grade: " Third year student",
+    desc: "I am currently pursuing an ICT Engineering degree at the Higher School Of Communication of Tunis, under the University of Carthage in Tunis, Tunisia.",
+    degree: "ICT Engineering degree",
+  },
+  {
+    id: 6,
+    img: "https://idaraty.s3.us-west-000.backblazeb2.com/logos/institut-preparatoire-aux-etudes-dingenieurs-d-el-manar.png",
+    school: "Preparatory Institute for Engineering Studies - IPEIEM",
+    date: "Sep 2019 - Jun 2021",
+    grade: " Rank 127 of more then 1500 students in national exam",
+    desc: "I completed my preparatory studies at the Preparatory Institute for Engineering Studies - IPEIEM, under the University of Tunis EL-MANAR in Tunisia.",
+    degree: "Preparatory School",
+  },
+];
+
+
+export const projects = [
+  {
+    id: 7,  // Ensure the id is unique
+    title: "SECNOLOGY MITRE Integration",
+    date: "Sep 2023 - Jan 2024",
+    description:
+      "Developed a comprehensive solution to integrate the MITRE ATT&CK framework into the SECNOLOGY SOAR platform. This project involved classifying various attack types, studying and analyzing known attacks, and proposing solutions for detection and mitigation. The integration aimed to enhance threat detection and response by automating OSquery data collection and parsing, developing detection rules aligned with MITRE ATT&CK, and creating interactive threat-hunting dashboards. The project addressed challenges in adapting to evolving threats and improving monitoring capabilities by leveraging advanced data collection methods and comprehensive threat intelligence.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/tuttored_project.png?alt=media&token=327cad4f-d1e1-4370-ac19-83539ff0565d", // Replace with actual image URL
+    tags: [
+      "MITRE ATT&CK",
+      "SOAR",
+      "OSquery",
+      "Sysmon",
+      "Threat Detection",
+      "Cybersecurity",
+      "Data Collection",
+    ],
+    category: "Security",
+    github: "https://github.com/fathi-belmkadem/secno-mitre-integration", // Replace with actual GitHub URL if available
+    webapp: "https://drive.google.com/file/d/1iFctk4i4Twnlp395S7Sn8Lz1ExHideLm/view?usp=sharing", // No web app URL available
+  }, 
+  {
+    id: 8,  // Ensure the id is unique
+    title: "P2M",
+    date: "Jan 2023 - May 2023",
+    description:
+      "Deployed ModSecurity as a reverse proxy near three containerized servers. Configured ModSecurity rulesets, resulting in a 70% reduction in web-based attacks, including SQL injection and cross-site scripting (XSS) incidents. Created a scalable architecture for safeguarding sensitive data and ensuring high availability. Conducted rigorous testing with the OWASP Juice Shop web application and applied a machine learning model to improve obfuscated packet detection by 45%.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/P2M.png?alt=media&token=c4020dad-b4b6-4589-a517-d5e7042b3e29", // Replace with actual image URL
+    tags: [
+      "ModSecurity",
+      "Docker",
+      "Kubernetes",
+      "Nginx",
+      "IIS",
+      "Firewall",
+      "XSS",
+      "SQL Injection",
+    ],
+    category: "Security",
+    github: "https://github.com/fathi-belmkadem/p2m", // Replace with actual GitHub URL
+    webapp: "https://drive.google.com/file/d/1jiwuS-vPwD1cbWbgsxBPkADVIOKPmlG_/view?usp=sharing", // No web app URL available
+  },
+  
+  {
+    id: 9,
+    title: "Mobile App",
+    date: "Sep 2022 - Dec 2022",
+    description:
+      "Created a mobile application for seamless control and real-time monitoring of an IoT weather station. Enhanced the user experience by providing a user-friendly interface that simplifies weather station control.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/MOBILE_APP.png?alt=media&token=619b80c0-1305-49f8-a7f2-e0d1765b2a00", // Replace with actual image URL
+    tags: [
+      "InfluxDB",
+      "Flutter",
+      "Laravel",
+      "Dart",
+    ],
+    category: "mobile app",
+    github: "https://github.com/fathi-belmkadem/mobile-project", // Replace with actual GitHub URL
+    webapp: "https://drive.google.com/file/d/1EGSJULseLURLeEjeu3_ha-cZdNTeymJZ/view?usp=sharing", // No web app URL available
+  },
+  {
+    id: 2,
+    title: "Desktop App",
+    date: "Jan 2022 - May 2022",
+    description:
+      "Developed a desktop application utilizing the Smith Abacus technique to calculate critical microwave characteristic values. Utilized advanced mathematical algorithms to enable precise microwave parameter calculations and analysis of complex microwave circuits.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-e9c5e.appspot.com/o/DESKTOP_APP.png?alt=media&token=f3e8af8f-248a-4bae-ac72-eaccac07fb64", 
+    tags: [
+      "QT5",
+      "Python",
+    ],
+    category: "desktop app",
+    github: "https://github.com/fathi-belmkadem/RF_TOOL", // Replace with actual GitHub URL
+    webapp: "", // No web app URL available
+  },
+];
